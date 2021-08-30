67aa5be94887730dad25095bc439d495fb4f4d90
Joshua Waiswa <jwaiswa7@gmail.com>
2021-08-30 18:25:45 +0300

    Using aws s3 for file uploads

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,16 +1,30 @@
 require "shrine"
 require "shrine/storage/file_system"
 require "shrine/storage/memory"
+require "shrine/storage/s3"
 Shrine.storages = if Rails.env.test?
                     {
                       cache: Shrine::Storage::Memory.new,
                       store: Shrine::Storage::Memory.new
                     }
-                  else
+                  elsif Rails.env.development?
                     {
                       cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
                       store: Shrine::Storage::FileSystem.new("public", prefix: "uploads") # permanent
                     }
+                  else 
+                    s3_options = {
+                      access_key_id: ENV["AWS_ID"],
+                      secret_access_key: ENV["AWS_SECRET"],
+                      region: ENV["AWS_UPLOAD_REGION"],
+                      bucket: ENV["AWS_BUCKET_NAME"]
+                    }
+
+                    Shrine.storages = {
+                      cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options), # temporary
+                      store: Shrine::Storage::S3.new(**s3_options) # permanent
+                    }
+
                   end
 Shrine.plugin :activerecord # loads Active Record integration
 Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
