8c172f4f4c7ea6f5cc5a92ce34864eefe1ee0e3a
Joshua Waiswa <jwaiswa7@gmail.com>
2021-08-30 17:53:31 +0300

    Adding sikdeiq url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
+  require "sidekiq/web"
+
+  mount Sidekiq::Web => "/sidekiq"  
   
   scope ":upload_id" do 
     resources :upload_errors, only: :index
